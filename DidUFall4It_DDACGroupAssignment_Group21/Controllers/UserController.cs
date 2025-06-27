4c168c69d8e45b980dc17fb3fb482968c9cdef89
kavis69 <mckavisgoddie@gmail.com>
2025-06-27 15:29:00 +0800

    Added cshtml and controller code for 3 features

---
+++ DidUFall4It_DDACGroupAssignment_Group21/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DidUFall4It_DDACGroupAssignment_Group21.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+
+        public IActionResult UserHome()
+        {
+            return View();
+        }
+
+        public IActionResult QuizAccess()
+        {
+            return RedirectToAction("Index", "Quiz");
+        }
+
+        public IActionResult InfographicAccess()
+        {
+            return RedirectToAction("ViewAll", "InfographicControllerUser");
+        }
+
+        public IActionResult ProgressAccess()
+        {
+            return RedirectToAction("List", "Progress");
+        }
+    }
+}
+
