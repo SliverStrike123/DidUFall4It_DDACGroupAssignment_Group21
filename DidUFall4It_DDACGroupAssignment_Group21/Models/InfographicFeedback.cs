5ee2ab57244e67e6b010565981c919a8408caae1
SliverStrike123 <teh.marvin@gmail.com>
2025-06-27 22:43:24 +0800

    mini infographic tings

--- DidUFall4It_DDACGroupAssignment_Group21/Models/InfographicFeedback.cs
+++ DidUFall4It_DDACGroupAssignment_Group21/Models/InfographicFeedback.cs
@@ -1,11 +1,17 @@
-﻿namespace DidUFall4It_DDACGroupAssignment_Group21.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DidUFall4It_DDACGroupAssignment_Group21.Models
 {
     public class InfographicFeedback
     {
         public int Id { get; set; }
         public string UserId { get; set; }
         public string InfographicTitle { get; set; }
-        public string Comment { get; set; }
+        public int InformativeRating { get; set; }
+        public int EngagementRating { get; set; }
+        public int ClarityRating { get; set; }
+        public int RelevanceRating { get; set; }
+        public string? Comment { get; set; }
         public DateTime PostedAt { get; set; }
     }
 
